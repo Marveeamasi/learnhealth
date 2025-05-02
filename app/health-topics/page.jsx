96ee4d8083f0ac8744be92898f6ff273362f2e55
Marvellous Amasi <102649743+Marveeamasi@users.noreply.github.com>
2025-05-02 01:40:21 +0100

    Update and rename page.jsx to page.jsx

--- app/health-topics/page.jsx
+++ app/health-topics/page.jsx
@@ -18,10 +18,10 @@ export default function HealthConditionPage() {
    <Navbar page={'health conditions'}/>
    <section className="px-30 py-20 flex flex-col gap-5">
     <div className="flex items-center gap-2 text-[16px] text-[#979797]">
-        <span>Home</span><RiArrowRightSLine/><span>Health Conditions</span><RiArrowRightSLine/><span>{selectedAlphabet}</span>
+        <span>Home</span><RiArrowRightSLine/><span>Health Topics</span><RiArrowRightSLine/><span>{selectedAlphabet}</span>
     </div>
-    <div className="font-[700] text-[48px]">Health Conditions & Topics</div>
-    <p className="font-[400] text-[16px] max-w-[455px]">Browse our vast range of health conditions and find information
+    <div className="font-[700] text-[48px]">Health Topics</div>
+    <p className="font-[400] text-[16px] max-w-[455px]">Browse our vast range of health topics and find information
     on treatment, prevention and coping strategies and stories.</p>
    </section>
    <AlphaSearch handleSetAlphabet={handleSetAlphabet}/>
