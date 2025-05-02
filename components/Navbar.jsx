e5cf2ea20091d38475448eee4c772c1b8decdffa
Marvellous Amasi <102649743+Marveeamasi@users.noreply.github.com>
2025-05-02 01:30:15 +0100

    Update Navbar.jsx

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -12,7 +12,7 @@ export default function Navbar({page}) {
       <Link href={'/'} className='text-[32px] max-sm:text-[24px] font-[700] cursor-pointer' style={{lineHeight: '30px'}}>Learn<br className='sm:hidden'/>Health</Link>
       <div className='flex w-[358px] justify-between font-[400] items-center max-sm:hidden'>
         <div className='flex items-center gap-[8px] cursor-pointer hover:font-semibold'>Discover</div>
-        <Link href={'/health-conditions'} className={`flex items-center gap-[8px] cursor-pointer hover:font-semibold`} style={{fontWeight: page==='health conditions' && '600'}}>Health Conditions</Link>
+        <Link href={'/health-topics'} className={`flex items-center gap-[8px] cursor-pointer hover:font-semibold`} style={{fontWeight: page==='health topics' && '600'}}>Health Topics</Link>
         <Link href={'#'} className='flex items-center gap-[8px] max-sm:hidden cursor-pointer hover:font-semibold'>About</Link>
       </div>
       <Link href={'#'} className='w-[132px] h-[37px] rounded-[24px] border border-[#000000] hover:bg-[#000000] max-sm:hidden hover:text-[#ffffff] text-sm cursor-pointer flex justify-center items-center transition-colors duration-300 ease-in-out'>Get in Touch!</Link>
