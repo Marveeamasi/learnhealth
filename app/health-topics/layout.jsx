e550ba938b33570b0c5a2cd10848f59414c0cecb
Marvellous Amasi <102649743+Marveeamasi@users.noreply.github.com>
2025-05-02 01:37:21 +0100

    Update and rename layout.jsx to layout.jsx

---
+++ app/health-topics/layout.jsx
@@ -0,0 +1,14 @@
+import "../globals.css";
+
+export const metadata = {
+  title: "Health Topics | LearnHealth",
+  description: "Browse our vast range of health topics and find information on treatment, prevention and coping strategies and stories.",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <>
+        {children}
+   </>
+  );
+}
